c214853b5122dc96a23c273e74f109fd6fa317d5
alpcanaydin <alpcan@alpcanaydin.com>
2017-07-15 09:37:06 +0300

    Redux types added.

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -3,6 +3,14 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import type { RouterState } from '../../types/router';
+
+export type ReduxState = {
+  router: RouterState,
+};
+
+export type ReduxActions = any;
+
 export default combineReducers({
   router: routerReducer,
 });
